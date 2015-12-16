2a71d9b91a2a0d4de71a47857d7ac2e3ba1c8e38
Antoine Lafouasse <antoine.lafouasse@icloud.com>
2015-12-16 11:35:09 +0100

    WIP

---
+++ src/test/java/fr/nate/anonymizer/ArgumentParserTest.java
@@ -0,0 +1,19 @@
+package fr.nate.anonymizer;
+
+import org.apache.commons.cli.ParseException;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Nate on 01/12/15.
+ */
+public class ArgumentParserTest {
+
+    @Test
+    public void theClsOptionShouldBeRequired() {
+        try {
+            ArgumentParser.parse(new String[] {});
+            Assert.fail();
+        } catch (ParseException e) {}
+    }
+}
